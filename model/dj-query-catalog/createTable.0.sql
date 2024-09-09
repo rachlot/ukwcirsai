d7d1b3be8addb2c9965534908cf0bea5f36f9b90
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-09-09 11:47:36 +0000

    Create a database with a cirs table #1

---
+++ model/dj-query-catalog/createTable.0.sql
@@ -0,0 +1,17 @@
+create table "Ereignis" (
+  ID serial primary key,
+  "Zuständiges Fachgebiet" text,
+  "Altersgruppe des Patienten" text,
+  "Wo ist das Ereignis passiert" text,
+  "Welche Versorgungsart" text,
+  "ASA-Klassifizierung" text,
+  "Patientenzustand" text,
+  "Wichtige Begleitumstände" text,
+  "War ein Medizinprodukt beteiligt" text,
+  "Fallbeschreibung" text,
+  "Was war besonders gut" text,
+  "Was war besonders ungünstig" text,
+  "Eigener Ratschlag" text,
+  "Wie häufig tritt ein Ereignis dieser Art auf" text,
+  "Ihre Berufserfahrung" text
+)
