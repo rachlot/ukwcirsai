8e15bd6b45abd7ce54cbe1abaad7c0ad27490baf
Andreas Eberhart <andreas.b.eberhart@gmail.com>
2024-09-10 10:00:03 +0000

    tabelle

--- model/dj-query-catalog/createTable.0.sql
+++ model/dj-query-catalog/createTable.0.sql
@@ -1,19 +1,16 @@
   create table  "Ereignis" (
   ID serial primary key,
   "Zuständiges Fachgebiet" text,
-  "Altersgruppe des Patienten" text,
   "Wo ist das Ereignis passiert" text,
-  "Welche Versorgungsart" text,
+  "Tag des Ereignisses (Wochentag oder Wochende)" text,
+  "Versorgungsart (Routinebetrieb oder Notfall)" text,
   "ASA-Klassifizierung" text,
   "Patientenzustand" text,
   "Wichtige Begleitumstände" text,
   "War ein Medizinprodukt beteiligt" text,
-  "Medizinprodukt" text, 
-  "Fallbeschreibung" text,
+  "Welches Medizinprodukt war beteiligt" text, 
   "Was war besonders gut" text,
   "Was war besonders ungünstig" text,
-  "Eigener Ratschlag" text,
   "Wie häufig tritt ein Ereignis dieser Art auf" text,
-  "Ihre Berufserfahrung" text,
-  "Name-Ersatz" text
+ 
 );
