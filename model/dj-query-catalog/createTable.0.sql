3b1b9d77405308990831e044e8b6e5728b8b0307
rachlot <charlotte.meyhardt.com>
2024-09-13 12:38:43 +0000

    add reporter role to system

--- model/dj-query-catalog/createTable.0.sql
+++ model/dj-query-catalog/createTable.0.sql
@@ -1,16 +1,19 @@
   create table  "Ereignis" (
   ID serial primary key,
+  "Berichtszeitpunkt" TIMESTAMP DEFAULT NOW(),
   "Zuständiges Fachgebiet" text,
   "Wo ist das Ereignis passiert" text,
-  "Tag des Ereignisses (Wochentag oder Wochende)" text,
-  "Versorgungsart (Routinebetrieb oder Notfall)" text,
-  "ASA-Klassifizierung" text,
-  "Patientenzustand" text,
+  "Tag des Ereignisses" text,
+  "Versorgungsart" text,
+  "ASA-Klassifizierung"  text,
+  "Patientenzustand"  text,
   "Wichtige Begleitumstände" text,
   "War ein Medizinprodukt beteiligt" text,
-  "Welches Medizinprodukt war beteiligt" text, 
+  "Welches Medizinprodukt war beteiligt" text,
+  "Fallbeschreibung" text,
   "Was war besonders gut" text,
   "Was war besonders ungünstig" text,
   "Wie häufig tritt ein Ereignis dieser Art auf" text,
- 
+  "Rolle des Berichtenden" text
 );
+  
